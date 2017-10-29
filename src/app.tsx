00c1bac725f4f8658431087ca65a1ef3921e0534
Brett Koenig <koenig.337@osu.edu>
2017-10-29 08:18:32 -0400

    simple react/redux click counter example working

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,29 @@
-
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Hello from "./Hello";
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <Hello name="Brett" />,
-  document.getElementById("root")
-);
+//Reducer - specifies how next state is calculated 
+//based on current state and action being dispatched
+const counter = (state = 0, action: any) => {
+    switch (action.type){
+        case 'INCREMENT':
+        return state + 1;
+        case 'DECREMENT':
+        return state - 1;
+        default:
+        return state;
+    }
+}
+
+const store = createStore(counter);
+
+const render = () => {
+	document.body.innerText = String(store.getState());
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+	store.dispatch({type: 'INCREMENT' });
+});
