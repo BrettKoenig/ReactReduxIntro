76d5be063739378d108cc494188a8ff6862a59c7
Brett Koenig <koenig.337@osu.edu>
2017-10-29 08:31:52 -0400

    after video 8

--- src/app.tsx
+++ src/app.tsx
@@ -15,15 +15,42 @@ const counter = (state = 0, action: any) => {
     }
 }
 
+//Counter component - dumb component, does not contain business logic - 
+//only specifies how current state transforms into renderable output 
+//and how callbacks are bound to event handlers
+const Counter = ({
+    value, 
+    onIncrement,
+    onDecrement
+}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+);
+
 const store = createStore(counter);
 
 const render = () => {
-	document.body.innerText = String(store.getState());
+	    ReactDOM.render(
+        //when we render a counter we specify values should be taken from Redux current state, 
+        //when user clicks button we dispatch corresponding action
+        <Counter value={store.getState()}
+        onIncrement={() =>
+            store.dispatch({
+                type:'INCREMENT'
+            })
+        }
+        onDecrement={() =>
+            store.dispatch({
+                type:'DECREMENT'
+            })
+        }
+        />, 
+        document.getElementById('root')
+    );
 };
 
 store.subscribe(render);
 render();
-
-document.addEventListener('click', () => {
-	store.dispatch({type: 'INCREMENT' });
-});
